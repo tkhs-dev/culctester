5ca15613ffeea459e30acb08cb0f71c6f0fe7abf
tkhs <tokuhausu@gmail.com>
2023-10-13 00:14:57 +0900

    Formulaを分離

---
+++ src/jvmMain/kotlin/logic/Formula.kt
@@ -0,0 +1,45 @@
+package logic
+
+class Formula(private val operand1: Operand, private val operand2: Operand?, private val operator: Operator?, var isNested:Boolean = false) {
+    interface Element
+    sealed class Operator:Element{
+        object Add: Operator()
+        object Sub: Operator()
+        object Mul: Operator()
+        object Div: Operator()
+    }
+    sealed class Operand:Element{
+        data class Number(val value: Int): Operand(){
+            override fun toString(): String {
+                if(value < 0)
+                    return "($value)"
+                else
+                    return value.toString()
+            }
+        }
+        data class Formula(val formula: logic.Formula): Operand(){
+            override fun toString(): String {
+                return formula.toString()
+            }
+        }
+    }
+
+    override fun toString(): String {
+        return with(StringBuilder()){
+            if (isNested) append("(")
+            append(operand1)
+            if (operator != null && operand2 != null){
+                append(
+                    when(operator){
+                        Operator.Add -> "+"
+                        Operator.Div -> "/"
+                        Operator.Mul -> "*"
+                        Operator.Sub -> "-"
+                    })
+                append(operand2)
+            }
+            if (isNested) append(")")
+            toString()
+        }
+    }
+}
